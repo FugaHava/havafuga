0c44967c304cc6ed1d738a09ea5fc674a196a257
Hava Fuga <hava.fuga@students.fhnw.ch>
2023-08-08 16:06:32 +0200

    init

---
+++ hava_2022/js/scripts.js
@@ -0,0 +1,10 @@
+// Empty JS for your own code to be here
+(function($){
+    $("#example").typer({
+        strings: [
+            "is a great free jQuery plugin",
+            "animates typing string",
+            "is hosted in jQueryScript.net"
+        ]
+    });
+}(jQuery || window.jQuery));
